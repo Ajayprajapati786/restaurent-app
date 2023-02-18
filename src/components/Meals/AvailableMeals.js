7b1ce6a46eb550779d5db565be5d3b400909a510
Ajay Prajapati <prajapatisani9@gmail.com>
2023-02-18 18:51:38 +0530

    135 by me

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,43 @@
-import classes from './AvailableMeals.module.css'
+import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
-const AvailableMeals = () =>{
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => {
+    return (<div><h1 style={{color:'black'}}>{meal.name}</h1>  <p style={{fontStyle:'italic'}}>{meal.description}</p> <p style={{color:'brown',fontWeight:'900'}}> $ {meal.price}</p> </div>);
+  });
 
-    return <section className={classes.meals}>
-        <ul>
-            {mealsList}
-        </ul>
+  return (
+    <section className={classes.meals}>
+      <ul style={{background:'white',padding:'10px', borderRadius:'15px'}}>{mealsList}</ul>
+      {console.log(mealsList)}
     </section>
-
+  );
 };
 
 export default AvailableMeals;
